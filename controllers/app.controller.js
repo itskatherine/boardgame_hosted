5a798c52b2c9764bd426f2ae122b80a4c715a306
itskatherine <hurstkatherine@hotmail.co.uk>
2022-05-16 11:41:01 +0100

    setup app model and controller files

---
+++ controllers/app.controller.js
@@ -0,0 +1,5 @@
+const { fetchCategories } = require("../models/app.model.js");
+
+getCategories = (req, res) => {};
+
+module.exports = { getCategories };
---
+++ models/app.model.js
@@ -0,0 +1,5 @@
+const db = require("../db/index");
+
+fetchCategories = () => {};
+
+module.exports = { fetchCategories };
