06b8d9809125c0357a6729b27b39bc17998b5e2e
itskatherine <hurstkatherine@hotmail.co.uk>
2022-05-18 15:47:31 +0100

    Added test for review object structure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -164,15 +164,24 @@ describe("PATCH /api/reviews/:review_id", () => {
 });
 
 describe("GET /api/reviews/:review_id/comments", () => {
-  test("200: Responds with an array of comments for review when given valid review id", () => {
+  test("200: Responds with an array of comments for review when given valid review id (for a review with comments)", () => {
     return request(app)
       .get("/api/reviews/2/comments")
       .expect(200)
       .then((response) => {
         const { comments } = response.body;
         expect(comments.length).toBe(3);
+        comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            review_id: expect.any(Number),
+          });
+        });
       });
-    //3
   });
 });
 
