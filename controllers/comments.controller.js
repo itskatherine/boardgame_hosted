f05c7207e172fcc3834663f57af757677aec9108
itskatherine <hurstkatherine@hotmail.co.uk>
2022-05-20 11:08:17 +0100

    added error handling for deleting comments

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,9 +2,11 @@ const { removeComment } = require("../models/comments.model");
 
 const deleteComment = (req, res, next) => {
   const id = req.params.comment_id;
-  removeComment(id).then(() => {
-    res.status(204).send();
-  });
+  removeComment(id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch(next);
 };
 
 module.exports = { deleteComment };
